e298b1e3261f85b46c958dcf574d254786065489
AlfaRiza <malfariza45@gmail.com>
2024-12-11 11:52:12 +0700

    feat: get token

--- src/cardless_reversal.php
+++ src/cardless_reversal.php
@@ -1,21 +1,32 @@
 <?php
 
+use BRI\CardlessCashWithdrawal\AuthToken;
 use BRI\CardlessCashWithdrawal\CardlessReversal;
 
 require __DIR__ . '/../../briapi-sdk/autoload.php';
 
-$secretKey = 'super_secret';
 $clientId = 'your_client_id';
 
 // url path values
 $baseUrl = 'https://api.bridex.qore.page/mock'; //base url
+$providerId = 'client_credentials'; // customer key
+$secretKey = 'S7zgRMA0rUMf4ddkagpreoECgYEAxRkh'; // customer secret
+
+$getToken = (new AuthToken())->authToken(
+  $baseUrl,
+  $providerId,
+  $secretKey
+);
+
+$accessToken = json_decode($getToken, true)['access_token'];
 
 $cardlessReversal = new CardlessReversal();
 
 $response = $cardlessReversal->cardlessReversal(
   $baseUrl,
   $clientId,
-  $secretKey
+  $secretKey,
+  $accessToken
 );
 
 echo $response;
--- src/cardless_withdrawal.php
+++ src/cardless_withdrawal.php
@@ -1,21 +1,32 @@
 <?php
 
+use BRI\CardlessCashWithdrawal\AuthToken;
 use BRI\CardlessCashWithdrawal\CardlessWithdrawal;
 
 require __DIR__ . '/../../briapi-sdk/autoload.php';
 
-$secretKey = 'super_secret';
 $clientId = 'your_client_id';
 
 // url path values
 $baseUrl = 'https://api.bridex.qore.page/mock'; //base url
+$providerId = 'client_credentials'; // customer key
+$secretKey = 'S7zgRMA0rUMf4ddkagpreoECgYEAxRkh'; // customer secret
+
+$getToken = (new AuthToken())->authToken(
+  $baseUrl,
+  $providerId,
+  $secretKey
+);
+
+$accessToken = json_decode($getToken, true)['access_token'];
 
 $cardlessWithDrawal = new CardlessWithdrawal();
 
 $response = $cardlessWithDrawal->cardlessWithdrawal(
   $baseUrl,
   $clientId,
-  $secretKey
+  $secretKey,
+  $accessToken
 );
 
 echo $response;
